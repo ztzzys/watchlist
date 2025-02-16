4fea83bcfafae1ae2c933148a96f85acb88d302b
ztzzys <1438814369@qq.com>
2025-02-16 17:24:02 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 
+import os
+import sys
+import click
+
 from markupsafe import escape
 from flask import url_for
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 app=Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
 
 
-name = 'ztzzys'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+
 
 
 @app.route('/user/<name>')
@@ -28,7 +36,6 @@ def user_page(name):
 
 @app.route('/test')
 def test_url_for():
-    print(url_for('hello'))
     print(url_for('user_page',name='ztzzys'))
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
@@ -36,4 +43,42 @@ def test_url_for():
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+       user=User.query.first()
+       movies=Movie.query.all()
+       return render_template('index.html',user=user,movies=movies)
+
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop.')
+def initdb(drop):
+    if drop:
+       db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'ztzzys'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+         movie=Movie(title=m['title'],year=m['year'])
+         db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
