39cf9e9032eef539985a6396dcf88725f267e67a
ztzzys <1438814369@qq.com>
2025-02-16 12:31:02 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+
 from markupsafe import escape
 from flask import url_for
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+name = 'ztzzys'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -20,3 +34,6 @@ def test_url_for():
     print(url_for('test_url_for',num=2))
     return 'Test page'
 
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
